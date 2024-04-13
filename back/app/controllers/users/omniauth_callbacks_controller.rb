648eac2f8af7b43621441e231856c247d6f8b5be
Kanae-Nishina <karubona.ra247@gmail.com>
2024-04-13 14:19:31 +0900

    Google認証とログアウト

--- back/app/controllers/users/omniauth_callbacks_controller.rb
+++ back/app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,17 +6,21 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
     user = User.from_omniauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      sign_in(:user, user, store: false, bypass: false)
-      token = user.create_token
-      user.tokens[token.client] = {
-        token: token.token,
-        token_hash: token.token_hash,
-        expiry: token.expiry
+      sign_in(user)
+      # トークンを生成
+      client_id = SecureRandom.urlsafe_base64(nil, false)
+      token     = SecureRandom.urlsafe_base64(nil, false)
+      token_hash = BCrypt::Password.create(token)
+      expiry    = (Time.now + DeviseTokenAuth.token_lifespan).to_i
+
+      user.tokens[client_id] = {
+        token: token_hash,
+        expiry: expiry
       }
+
       if user.save
-        redirect_to "#{ENV['FRONT_URL']}/ja/tasks?uid=#{user.uid}&token=#{token.token}&client=#{token.client}&expiry=#{token.expiry}", allow_other_host: true
+        redirect_to "#{ENV['FRONT_URL']}/ja/tasks?uid=#{user.uid}&token=#{token}&client=#{client_id}&expiry=#{expiry}", allow_other_host: true
       else
-        Rails.logger.error("Failed to save user: #{user.errors.full_messages.join(", ")}")
         redirect_to "#{ENV['FRONT_URL']}/?status=error", allow_other_host: true
       end
     else
